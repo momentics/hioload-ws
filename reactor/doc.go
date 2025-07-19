0f88a65e3502b6c29305deea5bb0314390613a30
momentics <momentics@gmail.com>
2025-07-19 06:39:41 +0300

    Delete reactor directory

--- reactor/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2025
-// Author: momentics <momentics@gmail.com>
-
-// Package reactor provides the core poll-mode event reactor abstraction and cross-platform implementations for epoll (Linux) and IOCP (Windows).
-package reactor
