33974b8c9251eec74fedfdc50d0bbff0ec1ce2c3
momentics <momentics@gmail.com>
2025-07-19 06:40:22 +0300

    Delete transport directory

--- transport/tcp/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2025
-// Author: momentics <momentics@gmail.com>
-
-// Package tcp implements the low-level TCP server and basic WebSocket handshake logic for hioload-ws.
-// Provides hook points and extensibility for advanced optimizations.
-package tcp
